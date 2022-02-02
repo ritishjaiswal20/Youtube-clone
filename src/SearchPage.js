8b7a6fb81a4dfbdb3009ed1837b09f2e749972c3
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-02-02 19:39:28 +0530

    search page added

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './SearchPage.css'
 import TuneOutlinedIcon from '@mui/icons-material/TuneOutlined';
 import ChannelRow from './ChannelRow.js'
+import VideoRow from './VideoRow';
 function SearchPage() {
   return (
   <div className="searchpage">
@@ -17,11 +18,24 @@ function SearchPage() {
         verified
         subs="660k"
         noOfVideos={382}
-        description="you can fid programe"
+        description="you can find programe"
      
      />
 
   <hr/>
+   
+   <VideoRow
+   image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRi-1IunumN5q6Jj9-RfLcomUf8BwqPKeGAw&usqp=CAU"
+   channel="Ritish jaiswal"
+   verified
+   subs="660k"
+   noOfVideos={382}
+   description="you can find programe"
+   title="build YT thumbnail"
+   timestamp="60 sec ago"
+   />
+
+
 
   </div>
   );
